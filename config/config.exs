311147ddbf86421d81c0d0f6936a0d18d4d365d6
Dayton Nolan <me@daytonnolan.com>
2025-06-27 17:04:12 -0700

    fix: remove git_ops, just do it manually

--- config/config.exs
+++ config/config.exs
@@ -1,16 +1,5 @@
 import Config
 
-config :git_ops,
-  mix_project: Mix.Project.get!(),
-  changelog_file: "CHANGELOG.md",
-  github_token: System.get_env("GITHUB_TOKEN"),
-  types: [tidbit: [hidden?: true], important: [header: "Important Changes"]],
-  github_handle_lookup?: true,
-  repository_url: "https://github.com/daytonn/ecto_cooler",
-  version_tag_prefix: "v",
-  manage_mix_version?: true,
-  manage_readme_version: true
-
 config :ecto_cooler, env: config_env()
 
 import_config "#{config_env()}.exs"
